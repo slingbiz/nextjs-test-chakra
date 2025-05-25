9157850a0b8d015fd6e06361d0e8d85ade7505b3
Ankur Pata <ankur.patankar@timedoctor.com>
2025-05-26 00:46:48 +0400

    Baloon.dev: Completed BD-33

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ const Index = () => (
     <Hero />
     <Main>
       <Text color="text">
-        This is a sample demo project in Baloon.dev. You can setup your own project and make changes direclty.{" "}
+        This is a sample demo project in baloon.dev. You can setup your own project and make changes direclty.{" "}
       </Text>
 
       <List spacing={3} my={0} color="text">
@@ -32,7 +32,7 @@ const Index = () => (
             flexGrow={1}
             mr={2}
           >
-            Editable with Baloon.dev <LinkIcon />
+            Editable with baloon.dev <LinkIcon />
           </ChakraLink>
         </ListItem>
         <ListItem>
@@ -46,7 +46,7 @@ const Index = () => (
 
     <DarkModeSwitch />
     <Footer>
-      <Text>Your Code ❤️ Baloon.dev</Text>
+      <Text>Your Code ❤️ baloon.dev</Text>
     </Footer>
     <CTA />
   </Container>
